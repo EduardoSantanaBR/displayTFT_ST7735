d85fcd727b95af86009a6267f8ffc7608bbbbba5
EduardoSantanaBR <edu_element190@hotmail.com>
2017-04-01 23:51:08 -0300

    Create tftST7735.c

---
+++ tftST7735.c
@@ -0,0 +1,19 @@
+/*
+*   tftST7735.c - Consta todas as funcoes necessarias para configuracao e inicializacao do ST7735
+*
+*   Criado: 01/04/2017
+*
+*   Autor: Eduardo Santana
+*/
+
+
+void ST7735_Init(void)
+{
+
+
+}
+
+void ST7735_Config(uint8 teste)
+{
+
+}
